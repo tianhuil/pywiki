38de1ebef77445d90ad685bacfbf57bed7ed9796
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2021-01-04 07:58:25 +0000

    Tweak names of variables

--- tasks.py
+++ tasks.py
@@ -5,30 +5,30 @@ from invoke import task
 # wikipedia appears to be dumped on the 1st and 20th of each month
 # we will pull the dump from the 1st
 now = datetime.utcnow()
-datestr = now.replace(day=1).strftime("%Y%m%d")
+first_of_month = now.replace(day=1).strftime("%Y%m%d")
 
 
 @task
-def download(ctx, datestr=datestr):
-    url = f"https://dumps.wikimedia.org/simplewiki/{datestr}/simplewiki-{datestr}-pages-meta-current.xml.bz2"
-    ctx.run(f"curl {url} -o scratch/simplewiki-{datestr}.xml.bz2", echo=True)
+def download(ctx, date=first_of_month):
+    url = f"https://dumps.wikimedia.org/simplewiki/{date}/simplewiki-{date}-pages-meta-current.xml.bz2"
+    ctx.run(f"curl {url} -o scratch/simplewiki-{date}.xml.bz2", echo=True)
 
 
 @task
-def wordcount(ctx, datestr=datestr):
+def wordcount(ctx, date=first_of_month):
     ctx.run(
         f"source env/bin/activate &&"
         f"python script/wordcount.py"
         f"  -m 5"
         f"  -s"
-        f"  scratch/simplewiki-{datestr}.xml.bz2"
+        f"  scratch/simplewiki-{date}.xml.bz2"
         f"  scratch/simplewiki-count.csv.gz",
         echo=True,
     )
 
 
 @task
-def update(ctx, datestr=datestr):
-    download(ctx, datestr)
-    wordcount(ctx, datestr)
+def update(ctx, date=first_of_month):
+    download(ctx, date)
+    wordcount(ctx, date)
     ctx.run("gunzip -c scratch/simplewiki-count.csv.gz | head -n 20", echo=True)
